12eb2df2483f6cb4bfab40de81ec62737e266261
TruxW <truxwood@gmail.com>
2025-02-24 10:50:42 -0700

    Add video Link
    For the reference to the video used for the states

--- Assets/_VXR_Win25_TruxWood/NameOfPattern (Enemy AI)/Scripts/State.cs
+++ Assets/_VXR_Win25_TruxWood/NameOfPattern (Enemy AI)/Scripts/State.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 
 public abstract class State : MonoBehaviour
+
+// VideoSource: https://www.youtube.com/watch?v=cnpJtheBLLY
+
 {
     public abstract State RunCurrentState();
 }
